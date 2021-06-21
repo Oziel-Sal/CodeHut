71e8082d2ea49e7767af3ec60f94bd75348917f6
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-20 22:00:56 -0500

    adding backend file and frontend file. Made some personal fiexes with some files

---
+++ frontend/src/components/Team.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class TeamMembers extends React.Component {
+    render(){
+        return(
+            <h1>About Me</h1>
+        )
+    }
+}
+export default TeamMembers;
---
+++ frontend/src/views/Landingpage.js
@@ -0,0 +1,13 @@
+import Description from "../components/Description";
+import TeamMembers from "../components/Team";
+
+function LandingPage(){
+    return(
+        <>
+        <Description/>
+        <TeamMembers/>
+        </>
+    );
+}
+
+export default LandingPage;
