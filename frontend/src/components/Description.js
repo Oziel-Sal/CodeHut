fbd19547e8356460b70172825407335950d44867
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-22 17:50:44 -0500

    testing random api calls from free api

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import "../style/Description.css"
+import axios from 'axios'
 
 class Description extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            anime: []
+        }
+    }
+
+
+
+    componentDidMount() {
+        console.log('');
+        axios.get('http://localhost:8080/randomAnime')
+            .then(res => {
+                console.log('inside axios');
+                this.setState({
+                    anime: res.data.anime,
+                    character: res.data.character,
+                    quote: res.data.quote
+
+                })
+            })
+        console.log('after axios');
+
+    }
+
+    //API call to random anime
+
+
+
     render() {
+
+        let Display = this.state.anime
+        let Display2 = this.state.character
+        let Display3 = this.state.quote
         return (
-                <p id="description">
-                    Code Hut is a way to bring help towards upcoming/experienced developers who struggle
-                    with finding a project to work on. You can find a variety of app ideas to choose for your own personal project!
-                </p>
+            <ul>
+                <h1><li>{Display}</li></h1>
+                <h1><li>{Display2}</li></h1>
+                <h1><li>{Display3}</li></h1>
+            </ul>
         )
     }
 }
