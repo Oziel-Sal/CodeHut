00e9e4a7a43c68220e5a7a7965697435a8a55e86
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-24 14:23:52 -0500

    added a team member component

--- frontend/src/views/Landingpage.js
+++ frontend/src/views/Landingpage.js
@@ -1,8 +1,10 @@
 import Description from "../components/Description";
+import TeamMembers from "../components/Team";
 function LandingPage(){
     return(
         <>
         <Description/>
+        <TeamMembers/>
         </>
     );
 }
