16dbdcf2f4606caafb088343afd7fc422980ff34
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-24 14:24:54 -0500

    Updated Team.js that displays a React Card that will enventually have my info

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Card, CardTitle, Row, Col, CardImg, Container,CardBody} from 'reactstrap';
+import '../style/Team.css'
 
 class TeamMembers extends React.Component {
-    render(){
-        return(
-            <h1>About Me</h1>
+    render() {
+        return (
+            <Container className="teamcontainer">
+                <h2 className="head_title">About Me</h2>
+                <Row>
+                    <Col sm="12" md="4">
+                        <Card>
+                            <CardImg bottom width="100%" src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="Card image camp" className="img" />
+                            <CardBody className="teambackground">
+                                <CardTitle className="teamtitle" tag="h5">Something</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
