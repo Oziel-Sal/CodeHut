ce5e0a8121b1dbc11f53f99b455a7e219c3fa9c5
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-27 14:09:59 -0500

    added tools commonent to my landing page

--- frontend/src/views/Landingpage.js
+++ frontend/src/views/Landingpage.js
@@ -1,9 +1,11 @@
 import Description from "../components/Description";
 import TeamMembers from "../components/Team";
+import Tools from "../components/Tools";
 function LandingPage(){
     return(
         <>
         <Description/>
+        <Tools/>
         <TeamMembers/>
         </>
     );
