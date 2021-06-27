84b59ab0f1f5c791581a2da2b1fee13564e96fb4
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-27 01:45:05 -0500

    fixed team card img

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Card, CardTitle, Row, Col, CardImg, Container,CardBody} from 'reactstrap';
+import { Card, CardTitle, Row, Col, CardImg, Container, CardBody, CardText, CardLink } from 'reactstrap';
 import '../style/Team.css'
 
 class TeamMembers extends React.Component {
     render() {
         return (
-            <Container className="teamcontainer">
-                <h2 className="head_title">About Me</h2>
-                <Row>
-                    <Col sm="12" md="4">
-                        <Card>
-                            <CardImg bottom width="100%" src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="Card image camp" className="img" />
-                            <CardBody className="teambackground">
-                                <CardTitle className="teamtitle" tag="h5">Something</CardTitle>
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
+
+            
+            <Container className="d-flex vh-100">
+                {
+                    <Row className="m-auto align-self-center">
+                        <Col>
+                            <Card className="card">
+                                <CardImg bottom width="100%" src="https://media-exp3.licdn.com/dms/image/C4E03AQG4we6AICc0tQ/profile-displayphoto-shrink_400_400/0/1565284114929?e=1629936000&v=beta&t=IHM_pmNPL6XIPMzL0ZTKofdnzdgSRXKjp9zQ2gB1DXI" alt="Card image camp" className="img" />
+                                <CardBody className="teambackground">
+                                    <CardTitle className="teamtitle" tag="h4">Oziel Salinas</CardTitle>
+                                    <CardText className="teamtext" tag="h5">Undergraduate Student</CardText>
+                                    <CardText className="teamtext" tag="h5">University of Texas Rio Grande Valley</CardText>
+                                    <CardLink className="memLink" href="https://www.linkedin.com/in/oziel-salinas-8a5a5418b/">Linkedin</CardLink>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                }
             </Container>
         )
     }
 }
 export default TeamMembers;
+
+
+//https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg
