68e5ac2457a1b4ec93db80181536d8ed637b7b43
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-27 14:08:35 -0500

    added a tools component to show which tools used for this project

---
+++ frontend/src/components/Tools.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../style/Tools.css'
+
+class Tools extends React.Component{
+    render(){
+        return(
+            <h1 className="tools_i">Tools</h1>
+        )
+    }
+}
+
+export default Tools;
