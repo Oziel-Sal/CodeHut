881c4b629076610632f70fe835cdde0df45ad10b
Oziel-Sal <oziel.salinas22@gmail.com>
2021-06-28 10:45:19 -0500

    deleted items inside tools.js and landingpage.js

--- frontend/src/components/Tools.js
+++ frontend/src/components/Tools.js
@@ -2,11 +2,7 @@ import React from "react";
 import '../style/Tools.css'
 
 class Tools extends React.Component{
-    render(){
-        return(
-            <h1 className="tools_i">Tools</h1>
-        )
-    }
+
 }
 
-export default Tools;
+export default Tools
--- frontend/src/views/Landingpage.js
+++ frontend/src/views/Landingpage.js
@@ -1,11 +1,9 @@
 import Description from "../components/Description";
 import TeamMembers from "../components/Team";
-import Tools from "../components/Tools";
 function LandingPage(){
     return(
         <>
         <Description/>
-        <Tools/>
         <TeamMembers/>
         </>
     );
