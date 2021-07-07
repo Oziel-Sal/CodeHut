e1a34c703ab1772cea3dce0f535da23678152e85
Oziel-Sal <oziel.salinas22@gmail.com>
2021-07-06 22:59:41 -0500

    DisplaySavedIdeas.js/css updated to display a table of data from database

--- frontend/src/components/DisplaySavedIdeas.js
+++ frontend/src/components/DisplaySavedIdeas.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../style/DisplaySavedIdeas.css'
 import axios from 'axios'
+import {
+    Row, Col, Container
+} from 'reactstrap';
 
 
 class SavedIdeas extends React.Component {
@@ -10,6 +13,8 @@ class SavedIdeas extends React.Component {
             data: []
         }
     }
+
+    //Everytime Page is refreshed it will get ALL Reddit Author, Title, Description
     componentDidMount() {
         axios.get('http://localhost:8080/getAll')
             .then(res => {
@@ -22,16 +27,44 @@ class SavedIdeas extends React.Component {
     }
     render() {
         let Data = this.state.data
-        console.log(Data)
         return (
             <>
-                <ul>
-                    {
-                        Data.map((data, index) => (
-                            <li key={index} className="white">{data}</li>
-                        ))
-                    }
-                </ul>
+                <Container>
+                    <Row>
+                        <Col xs="4" sm="4" md="4" className="app_col">
+                            <p className="white">Author</p>
+                        </Col>
+                        <Col xs="4" sm="4" md="4" className="app_col">
+                            <p className="white">Title</p>
+                        </Col>
+
+                        <Col xs="4" sm="4" md="4" className="app_col">
+                            <p className="white">Description</p>
+
+                        </Col>
+
+                        {
+                            Data.map((data, index) => (
+                                <>
+                                    <Col xs="4" sm="4" md="4" key={index} className="app_col">
+                                        <p className="white">{data.authors}</p>
+                                    </Col>
+                                    <Col xs="4" sm="4" md="4" key={index} className="app_col">
+                                        <p className="white">{data.titles}</p>
+                                    </Col>
+
+                                    <Col xs="4" sm="4" md="4" key={index} className="app_col">
+                                        <p className="white">{data.description}</p>
+
+                                    </Col>
+                                </>
+
+                            ))
+                        }
+                    </Row>
+                </Container>
+
+
             </>
         )
     }
